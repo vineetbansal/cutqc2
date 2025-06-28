7e63139e264bd434e1d4892252d8cae7c02f41e3
Vineet Bansal <vineetbansal@protonmail.com>
2025-06-27 20:23:29 -0400

    disable cupy tests on CI

--- tests/test_cupy.py
+++ tests/test_cupy.py
@@ -1,7 +1,9 @@
 from cutqc2.cupy.simple import matrix_add, matrix_subtract
 import numpy as np
+import pytest
 
 
+@pytest.mark.skip(reason="Cannot run on Github CI currently")
 def test_matrix_add():
     a = np.array([[0, 1, 2, 3], [4, 5, 6, 7]], dtype=np.float32)
     b = np.array([[0, 3, 6, 9], [12, 15, 18, 21]], dtype=np.float32)
@@ -10,6 +12,7 @@ def test_matrix_add():
     assert np.allclose(result, expected)
 
 
+@pytest.mark.skip(reason="Cannot run on Github CI currently")
 def test_matrix_subtract():
     a = np.array([[0, 1, 2, 3], [4, 5, 6, 7]], dtype=np.float32)
     b = np.array([[0, 3, 6, 9], [12, 15, 18, 21]], dtype=np.float32)
