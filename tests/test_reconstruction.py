a4dbe7c45cdaf06c854319fc20f45053b1b5e45c
Vineet Bansal <vineetbansal@protonmail.com>
2025-07-15 20:23:11 -0400

    correct test case for read_dd_bins; testing against a simpler approach

--- tests/test_reconstruction.py
+++ tests/test_reconstruction.py
@@ -7,100 +7,85 @@ def test_reconstruction():
     Test reconstruction for a 5-qubit circuit with 2 subcircuits (with 2 and 3 qubits respectively).
     """
 
+    bins = np.array(
+        [
+            0.00,
+            0.10,
+            0.20,
+            0.30,
+            0.40,
+            0.50,
+            0.60,
+            0.70,
+            0.80,
+            0.90,
+            1.00,
+            1.10,
+            1.20,
+            1.30,
+            1.40,
+            1.50,
+        ]
+    )
+
     result = read_dd_bins(
         # Mapping from subcircuit index to qubit indices
-        # This is the actual (and consistent) mapping for Figure 4 in the paper
-        # TODO: Find out why.
-        subcircuit_out_qubits={0: [1, 0], 1: [2, 4, 3]},
+        subcircuit_out_qubits={0: [1, 0], 1: [2, 3]},
         # Mapping from recursion level to a dict of bin information
         dd_bins={
             0: {
                 # Mapping from subcircuit index to the state of its qubits (active/merged)
                 "subcircuit_state": {
                     0: ["active", "active"],
-                    1: ["active", "active", "active"],
+                    1: ["active", "active"],
                 },
                 # Ordering of subcircuits
                 "smart_order": [0, 1],
-                # 2^5 = 32 probability values
-                "bins": np.array(
-                    [
-                        0.10803459,
-                        0.0078125,
-                        0.0390625,
-                        0.03259041,
-                        0.07678459,
-                        0.0390625,
-                        0.0078125,
-                        0.06384041,
-                        0.00134041,
-                        0.0078125,
-                        0.0078125,
-                        0.04553459,
-                        0.00134041,
-                        0.0078125,
-                        0.0078125,
-                        0.04553459,
-                        0.00134041,
-                        0.0078125,
-                        0.0078125,
-                        0.04553459,
-                        0.00134041,
-                        0.0078125,
-                        0.0078125,
-                        0.04553459,
-                        0.07678459,
-                        0.0390625,
-                        0.0078125,
-                        0.06384041,
-                        0.10803459,
-                        0.0078125,
-                        0.0390625,
-                        0.03259041,
-                    ]
-                ),
+                # 2^4 = 16 probability values
+                # these are made-up values so we can easily keep track
+                # of where they get moved
+                "bins": bins,
                 "expanded_bins": [],
             }
         },
     )
 
-    # 2^5 = 32 "reconstructed" probability values
+    # 2^4 = 16 "reconstructed" probability values
     assert np.allclose(
         result,
         np.array(
             [
-                0.10803459,
-                0.00134041,
-                0.00134041,
-                0.07678459,
-                0.07678459,
-                0.00134041,
-                0.00134041,
-                0.10803459,
-                0.0078125,
-                0.0078125,
-                0.0078125,
-                0.0390625,
-                0.0390625,
-                0.0078125,
-                0.0078125,
-                0.0078125,
-                0.0390625,
-                0.0078125,
-                0.0078125,
-                0.0078125,
-                0.0078125,
-                0.0078125,
-                0.0078125,
-                0.0390625,
-                0.03259041,
-                0.04553459,
-                0.04553459,
-                0.06384041,
-                0.06384041,
-                0.04553459,
-                0.04553459,
-                0.03259041,
+                0.0,
+                0.4,
+                0.8,
+                1.2,
+                0.2,
+                0.6,
+                1.0,
+                1.4,
+                0.1,
+                0.5,
+                0.9,
+                1.3,
+                0.3,
+                0.7,
+                1.1,
+                1.5,
             ]
         ),
     )
+
+    # A simpler approach for the case where all the qubits are "active"
+    # would be:
+    def apply_lookup(n, L):
+        # Apply a lookup table to map the bits of n to the indices in L
+        output = 0
+        for i in range(len(L)):
+            if (n >> i) & 1:  # if the i-th bit of n is set
+                output |= 1 << L[i]  # set the bit in output at position L[i]
+        return output
+
+    indices = np.array([apply_lookup(i, [3, 2, 0, 1]) for i in range(16)])
+    expected_result = np.zeros(len(indices))
+    expected_result[indices] = bins
+    assert np.allclose(result, expected_result)
